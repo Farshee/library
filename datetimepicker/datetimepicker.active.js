2f2a3c0a7e2a17f8f39b56ecdc5e74dfaadfb9bd
salmanfarshee <salman.bdtask@gmail.com>
2022-04-28 15:38:53 +0600

    datetime picker add

---
+++ datetimepicker/datetimepicker.active.js
@@ -0,0 +1,14 @@
+//datepicker
+ $('.datepicker_time').datetimepicker({
+         timepicker: false,
+        format: 'Y-m-d'
+    });
+
+    $('.datetimepicker').datetimepicker({
+        format: "Y-m-d H:i"
+    });
+
+    $('.timePicker').datetimepicker({
+        datepicker: false,
+        format: "H:i"
+    });
